b5f61d194c5bec26781c242ae939caaf0c36d99a
Rune Berg <1runeberg@users.noreply.github.com>
2019-04-04 04:21:28 +1100

    New custom anim node: Set VR Wrist Transform - Sets the wrist transform of a target pose from a reference pose

---
+++ SteamVR/SteamVRInput/Source/SteamVRInputDevice/Private/AnimNode_SteamVRSetWristTransform.cpp
@@ -0,0 +1,87 @@
+/*
+Copyright 2019 Valve Corporation under https://opensource.org/licenses/BSD-3-Clause
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its contributors
+   may be used to endorse or promote products derived from this software
+   without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+*/
+
+
+#include "AnimNode_SteamVRSetWristTransform.h"
+#include "ISteamVRInputDeviceModule.h"
+#include "AnimationRuntime.h"
+#include "AnimInstanceProxy.h"
+#include "SteamVRInputDevice.h"
+#include "UE4HandSkeletonDefinition.h"
+
+FAnimNode_SteamVRSetWristTransform::FAnimNode_SteamVRSetWristTransform()
+{
+}
+
+void FAnimNode_SteamVRSetWristTransform::Initialize(const FAnimationInitializeContext& Context)
+{
+	// Initialize our poses
+	ReferencePose.Initialize(Context);
+	TargetPose.Initialize(Context);
+}
+
+void FAnimNode_SteamVRSetWristTransform::CacheBones(const FAnimationCacheBonesContext & Context)
+{
+}
+
+void FAnimNode_SteamVRSetWristTransform::Update(const FAnimationUpdateContext & Context)
+{
+	// Update our poses this frame
+	ReferencePose.Update(Context);
+	TargetPose.Update(Context);
+}
+
+void FAnimNode_SteamVRSetWristTransform::Evaluate(FPoseContext& Output)
+{
+	Output.ResetToRefPose();
+
+	// Set source bone indices
+	FCompactPoseBoneIndex RootBoneIndex = FCompactPoseBoneIndex(0);
+	FCompactPoseBoneIndex SteamVRWristBoneIndex = FCompactPoseBoneIndex(1);
+
+	// Apply all the bones from the target to the output pose
+	TargetPose.Evaluate(Output);
+
+	// Setup buffer for the reference pose
+	FPoseContext ReferenceContext = FPoseContext(Output.AnimInstanceProxy);
+	ReferencePose.Evaluate(ReferenceContext);
+
+	// Apply the appropriate root and/or wrist transforms to our output pose based on the skeleton being used
+	if (HandSkeleton == EHandSkeleton::VR_SteamVRHandSkeleton)
+	{
+		// Since we are dealing with the SteamVR Hand Skeleton, we need to set both the root and wrist Bones
+		Output.Pose[RootBoneIndex] = ReferenceContext.Pose[RootBoneIndex];
+		Output.Pose[SteamVRWristBoneIndex] = ReferenceContext.Pose[SteamVRWristBoneIndex];
+	}
+	else
+	{
+		// For the UE4 stock hand skeleton, we only need the root bone (which is the wrist bone in this skeleton)
+		Output.Pose[RootBoneIndex] = ReferenceContext.Pose[RootBoneIndex];
+	}
+}
---
+++ SteamVR/SteamVRInput/Source/SteamVRInputDevice/Public/AnimNode_SteamVRSetWristTransform.h
@@ -0,0 +1,70 @@
+/*
+Copyright 2019 Valve Corporation under https://opensource.org/licenses/BSD-3-Clause
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its contributors
+   may be used to endorse or promote products derived from this software
+   without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+*/
+
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimNodeBase.h"
+#include "SteamVRInputDeviceFunctionLibrary.h"
+#include "SteamVRSkeletonDefinition.h"
+#include "AnimNode_SteamVRSetWristTransform.generated.h"
+
+/**
+* Custom animation node that sets the wrist transform of a target pose from a reference pose
+*/
+USTRUCT()
+struct STEAMVRINPUTDEVICE_API FAnimNode_SteamVRSetWristTransform : public FAnimNode_Base
+{
+	GENERATED_USTRUCT_BODY()
+
+	/** The pose from where we will get the root and/or wrist transform from */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Links)
+	FPoseLink ReferencePose;
+
+	/** What kind of skeleton is used in the reference pose */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Settings, meta = (AlwaysAsPin))
+	EHandSkeleton HandSkeleton;
+
+	/** The pose to apply the wrist transform to */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Links)
+	FPoseLink TargetPose;
+
+public:
+
+	// FAnimNode_Base interface
+	virtual void Initialize(const FAnimationInitializeContext& Context) override;
+	virtual void CacheBones(const FAnimationCacheBonesContext & Context) override;
+	virtual void Update(const FAnimationUpdateContext & Context) override;
+	virtual void Evaluate(FPoseContext& Output) override;
+	// End of FAnimNode_Base interface
+
+	FAnimNode_SteamVRSetWristTransform();
+
+};
