927c4a315e0620bd23033254de8ebb3ddbf06c6c
Rune Berg <1runeberg@users.noreply.github.com>
2019-04-04 19:09:47 +1100

    Switch Device sig to release year (for now)

--- SteamVR/SteamVRInput/Source/SteamVRInputDevice/Public/SteamVRInputDevice.h
+++ SteamVR/SteamVRInput/Source/SteamVRInputDevice/Public/SteamVRInputDevice.h
@@ -258,7 +258,7 @@ public:
 	EVRSkeletalTrackingLevel RightControllerFidelity;
 
 	/** Device identifier used to check for class validity */
-	FString DeviceSignature;
+	int32 DeviceSignature = 0;
 
 private:
 #if WITH_EDITOR
