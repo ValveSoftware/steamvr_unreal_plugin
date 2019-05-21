b217f32fbac9b0d31557c493d63af6a0840c30fd
Rune Berg <runeb@valvesoftware.com>
2019-05-21 17:04:29 +1000

    Fix Issue#26: Ensure that we find a paired XY axis to create a Vector2 for actions that are meant to be generated for all controllers via the MotionController UE abstraction

--- SteamVRInput/Source/SteamVRInputDevice/Private/SteamVRInputDevice.cpp
+++ SteamVRInput/Source/SteamVRInputDevice/Private/SteamVRInputDevice.cpp
@@ -2792,9 +2792,13 @@ void FSteamVRInputDevice::ProcessKeyAxisMappings(const UInputSettings* InputSett
 			{
 				CurrentControllerType = FString(TEXT("Windows_MR"));
 			} 
+			else if (CurrentKey.Contains(TEXT("MotionController")))
+			{
+				// empty on purpose (readability)
+			}
 			else if (CurrentKey.Contains(TEXT("Input_Temporary")))
 			{
-				continue;
+				continue;	// explicit on purpose (readability)
 			}
 			else
 			{
