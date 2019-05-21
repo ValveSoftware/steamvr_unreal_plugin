5a00e19b51c6972bb3af00b596f937bf8f3493a0
Rune Berg <runeb@valvesoftware.com>
2019-05-21 14:38:36 +1000

    Enhancement #25: Expose BP "Reset Seated Position" node in function library

--- SteamVRInput/Source/SteamVRInputDevice/Public/SteamVRInputDeviceFunctionLibrary.h
+++ SteamVRInput/Source/SteamVRInputDevice/Public/SteamVRInputDeviceFunctionLibrary.h
@@ -470,6 +470,16 @@ public:
 	static bool FindSteamVR_ActionOrigin(FName ActionName, FName ActionSet = FName("main"));
 
 	/**
+	* Sets the zero pose for the seated tracker coordinate system to the current position and yaw of the HMD. 
+	* After this call, calls that pass TrackingUniverseSeated as the origin will be relative to this new zero pose.
+	*
+	* NOTE: This function overrides the user's previously saved seated zero pose and should only be called as the result of a user action.
+	* Users are also able to set their seated zero pose via the SteamVR Dashboard.
+	*/
+	UFUNCTION(BlueprintCallable, Category = "SteamVR Input")
+	static void ResetSeatedPosition();
+
+	/**
 	* Get the SteamVR Bone Transform value in UE coordinates
 	* @param SteamBoneTransform - The SteamVR Bone Transform value to get the UE coordinates for
 	*/
