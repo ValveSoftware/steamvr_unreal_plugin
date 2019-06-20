ac667d1c2f00edbe5d21b774660b7402092e1d07
Rune Berg <runeb@valvesoftware.com>
2019-06-20 14:36:52 +1000

    Enable to continually update tracking references & devices with the SteamVRTracking Reference Component, also covers enhancement #45

--- SteamVRInput/Source/SteamVRInputDevice/Public/SteamVRTrackingRefComponent.h
+++ SteamVRInput/Source/SteamVRInputDevice/Public/SteamVRTrackingRefComponent.h
@@ -34,6 +34,8 @@ POSSIBILITY OF SUCH DAMAGE.
 #include "Components/StaticMeshComponent.h"
 #include "SteamVRTrackingRefComponent.generated.h"
 
+// Delegates
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_ThreeParams(FComponentTrackingActivatedSignature, int32, DeviceID, FName, DeviceClass, FString, DeviceModel);
 
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class STEAMVRINPUTDEVICE_API USteamVRTrackingReferences : public UActorComponent
@@ -42,6 +44,11 @@ class STEAMVRINPUTDEVICE_API USteamVRTrackingReferences : public UActorComponent
 
 public:	
 	USteamVRTrackingReferences();
+
+	// Blueprint event - When a new active device is recognized
+	UPROPERTY(BlueprintAssignable, Category = "VR")
+	FComponentTrackingActivatedSignature OnTrackedDeviceActivated;
+
 
 	// TODO: Set default mesh to SteamVR provided render model. Must be backwards compatible to UE4.15
 
@@ -54,14 +61,22 @@ public:
 	void HideTrackingReferences();
 
 	/** Scale to apply to the tracking reference mesh */
-	UPROPERTY(BlueprintReadOnly, Category = "SteamVR Input")
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "SteamVR Input")
+	float ActiveDevicePollFrequency = 1.f;
+
+	/** Scale to apply to the tracking reference mesh */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "SteamVR Input")
 	FVector TrackingReferenceScale = FVector(1.f);
 
 	/** Currently displayed Tracking References in-world */
 	UPROPERTY(BlueprintReadOnly, Category = "SteamVR Input")
 	TArray<UStaticMeshComponent*> TrackingReferences;
 
+private:
+	float CurrentDeltaTime = 0.f;					
+	TArray<unsigned int> ActiveTrackingDevices;
+
 protected:
 	virtual void BeginPlay() override;	
-	
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 };
